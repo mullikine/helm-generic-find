07c5a9f81eaa3d1b9939affddc8ed9a4968d95d9
Shane Mulligan <mullikine@gmail.com>
2020-09-15 10:59:25 +1200

    Tue 15 Sep 2020 10:59:25 NZST

--- helm-generic-find.el
+++ helm-generic-find.el
@@ -15,27 +15,26 @@
 (defset helm-generic-find-source
   ;; generic find === name
   (helm-build-async-source "generic find"
-    :candidates-process 'helm-generic-find--do-candidate-process
+    :candidates-process (defun helm-generic-find--do-candidate-process ()
+                          (let* ((cmd-args (-filter 'identity
+                                                    (nconc (cmd2list helm-generic-find-cmd)
+                                                           (list
+                                                            ;; Pattern is provided by helm when the function is run
+                                                            helm-pattern))))
+                                 ;; helm generic find process === name
+                                 (proc (apply 'start-file-process "helm generic find process" helm-buffer cmd-args)))
+                            (prog1 proc
+                              (set-process-sentinel
+                               (get-buffer-process helm-buffer)
+                               #'(lambda (process event)
+                                   (helm-process-deferred-sentinel-hook
+                                    process event (helm-default-directory)))))))
     :filter-one-by-one 'identity
     ;; Don't let there be a minimum. it's annoying
     :requires-pattern 0
     :action 'helm-find-file-or-marked
     :candidate-number-limit 9999))
 
-(defun helm-generic-find--do-candidate-process ()
-  (let* ((cmd-args (-filter 'identity
-                            (nconc (cmd2list helm-generic-find-cmd)
-                                   (list
-                                    ;; Pattern is provided by helm when the function is run
-                                    helm-pattern))))
-         ;; helm generic find process === name
-         (proc (apply 'start-file-process "helm generic find process" helm-buffer cmd-args)))
-    (prog1 proc
-      (set-process-sentinel
-       (get-buffer-process helm-buffer)
-       #'(lambda (process event)
-         (helm-process-deferred-sentinel-hook
-          process event (helm-default-directory)))))))
 
 ;;;###autoload
 (defun helm-generic-find (cmd &optional directory)
