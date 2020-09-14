c47956a2d3152776a821b15c924998e9e596322b
Shane Mulligan <mullikine@gmail.com>
2020-09-15 11:32:38 +1200

    Tue 15 Sep 2020 11:32:38 NZST

--- helm-generic-find.el
+++ helm-generic-find.el
@@ -14,24 +14,24 @@
          (generic-find-cmd cmd)
          (generic-find-cmd-slug (slugify cmd))
          (generic-find-source-name (concat "helm generic " generic-find-cmd-slug))
-         (generic-find-func-name (concat "helm-generic-" generic-find-cmd-slug "--do-candidate-process"))
+         (generic-find-func-sym (str2sym (concat "helm-generic-" generic-find-cmd-slug "--do-candidate-process")))
          (generic-find-process-name (concat "helm generic " generic-find-cmd-slug " proc"))
          (generic-find-buffer-name (concat "*helm-" generic-find-cmd-slug "*")))
     (helm :sources (list (helm-build-async-source generic-find-source-name
-                           :candidates-process (defun helm-generic-find--do-candidate-process ()
-                                                 (let* ((cmd-args (-filter 'identity
-                                                                           (nconc (cmd2list
-                                                                                   (buffer-local-value 'generic-find-cmd (get-buffer helm-buffer)))
-                                                                                  (list
-                                                                                   ;; Pattern is provided by helm when the function is run
-                                                                                   helm-pattern))))
-                                                        (proc (apply 'start-file-process generic-find-process-name helm-buffer cmd-args)))
-                                                   (prog1 proc
-                                                     (set-process-sentinel
-                                                      (get-buffer-process helm-buffer)
-                                                      #'(lambda (process event)
-                                                          (helm-process-deferred-sentinel-hook
-                                                           process event (helm-default-directory)))))))
+                           :candidates-process (eval `(defun ,generic-find-func-sym ()
+                                                       (let* ((cmd-args (-filter 'identity
+                                                                                 (nconc (cmd2list
+                                                                                         (buffer-local-value 'generic-find-cmd (get-buffer helm-buffer)))
+                                                                                        (list
+                                                                                         ;; Pattern is provided by helm when the function is run
+                                                                                         helm-pattern))))
+                                                              (proc (apply 'start-file-process generic-find-process-name helm-buffer cmd-args)))
+                                                         (prog1 proc
+                                                           (set-process-sentinel
+                                                            (get-buffer-process helm-buffer)
+                                                            #'(lambda (process event)
+                                                                (helm-process-deferred-sentinel-hook
+                                                                 process event (helm-default-directory))))))))
                            :filter-one-by-one 'identity
                            ;; Don't let there be a minimum. it's annoying
                            :requires-pattern 0
