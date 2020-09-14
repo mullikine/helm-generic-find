30737ee75c7816cb72b9fc4e633473c6dd0ae853
Shane Mulligan <mullikine@gmail.com>
2020-09-15 11:14:04 +1200

    Tue 15 Sep 2020 11:14:04 NZST

--- helm-generic-find.el
+++ helm-generic-find.el
@@ -7,41 +7,35 @@
 ;; instantiate a helm with a custom backend
 ;; command
 
-(defcustom helm-generic-find-cmd "echo yo"
-  "Default executable for fzf"
-  :type 'stringp
-  :group 'helm-generic-find)
-
-(defset helm-generic-find-source
-  ;; generic find === name
-  (helm-build-async-source "generic find"
-    :candidates-process (defun helm-generic-find--do-candidate-process ()
-                          (let* ((cmd-args (-filter 'identity
-                                                    (nconc (cmd2list helm-generic-find-cmd)
-                                                           (list
-                                                            ;; Pattern is provided by helm when the function is run
-                                                            helm-pattern))))
-                                 ;; helm generic find process === name
-                                 (proc (apply 'start-file-process "helm generic find process" helm-buffer cmd-args)))
-                            (prog1 proc
-                              (set-process-sentinel
-                               (get-buffer-process helm-buffer)
-                               #'(lambda (process event)
-                                   (helm-process-deferred-sentinel-hook
-                                    process event (helm-default-directory)))))))
-    :filter-one-by-one 'identity
-    ;; Don't let there be a minimum. it's annoying
-    :requires-pattern 0
-    :action 'helm-find-file-or-marked
-    :candidate-number-limit 9999))
-
+(defun helm-generic-cmd ()
+  (buffer-local-value 'generic-find-cmd (get-buffer helm-buffer)))
 
 ;;;###autoload
-(defun helm-generic-find (cmd &optional directory)
+(defun helm-generic-find (cmd)
   (interactive "D")
-  (let ((default-directory directory)
-        (cmd cmd))
-    (helm :sources '(helm-generic-find-source)
+  (let* ((default-directory directory)
+        (generic-find-cmd cmd)
+        (generic-find-cmd-slug (slugify-cmd)))
+    (helm :sources (list (helm-build-async-source "generic find"
+                           :candidates-process (defun helm-generic-find--do-candidate-process ()
+                                                 (let* ((cmd-args (-filter 'identity
+                                                                           (nconc (cmd2list (helm-generic-cmd))
+                                                                                  (list
+                                                                                   ;; Pattern is provided by helm when the function is run
+                                                                                   helm-pattern))))
+                                                        ;; helm generic find process === name
+                                                        (proc (apply 'start-file-process "helm generic find process" helm-buffer cmd-args)))
+                                                   (prog1 proc
+                                                     (set-process-sentinel
+                                                      (get-buffer-process helm-buffer)
+                                                      #'(lambda (process event)
+                                                          (helm-process-deferred-sentinel-hook
+                                                           process event (helm-default-directory)))))))
+                           :filter-one-by-one 'identity
+                           ;; Don't let there be a minimum. it's annoying
+                           :requires-pattern 0
+                           :action 'helm-find-file-or-marked
+                           :candidate-number-limit 9999))
           :buffer "*helm-generic-find*")))
 
 (provide 'helm-generic-find)
